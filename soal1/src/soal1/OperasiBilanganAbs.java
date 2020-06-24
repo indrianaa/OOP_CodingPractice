ea4d25fc65a62520a58180eee8b29a4b628f12f9
indrianaa <38153788+indrianaa@users.noreply.github.com>
2020-06-24 15:17:48 +0700

    Add files via upload

---
+++ soal1/src/soal1/OperasiBilanganAbs.java
@@ -0,0 +1,17 @@
+package soal1;
+
+//abstract class
+public abstract class OperasiBilanganAbs {
+
+    //atribut abstract class
+    protected double a,b,c;
+
+    //method abstract class
+    protected abstract void set_A (double a);
+    protected abstract void set_B (double b);
+    protected abstract double set_C();
+    protected abstract double get_A();
+    protected abstract double get_B();
+    protected abstract double get_C();
+    protected abstract void tampil();
+}
